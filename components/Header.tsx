3c2a0d50bfb181168b6ca5fbb4d103b7cbf2a410
Sheikh-JamirAlam <sjamiralam@x-ack.com>
2026-01-09 00:05:35 +0530

    Add Vivo logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,13 @@ export default function Header() {
   return (
     <section className="grid grid-cols-3">
       <div className="w-160 h-19">
-        <Image src="/vivologo.png" alt="Vivo Logo" width={1280} height={152} className="object-cover object-center mx-auto" />
+        <Image src="/vivologo2.png" alt="Vivo Logo" width={1280} height={152} className="object-cover object-center mx-auto" />
       </div>
       <div>
         <Image src="/xaviesta.png" alt="Xaviesta Logo" width={3840} height={640} className="w-90 py-2 mx-auto" />
       </div>
       <div className="w-160 h-19">
-        <Image src="/vivologo.png" alt="Evereadylogo Logo" width={1280} height={152} className="object-cover object-center mx-auto" />
+        <Image src="/vivologo1.png" alt="Evereadylogo Logo" width={1280} height={152} className="object-cover object-center mx-auto" />
       </div>
     </section>
   );
