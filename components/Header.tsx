99e4e2f1bea5087386fa1128e142f280f1a894d8
Sheikh-JamirAlam <sjamiralam@x-ack.com>
2026-01-08 13:05:27 +0530

    Eveready Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,8 +9,8 @@ export default function Header() {
       <div>
         <Image src="/xaviesta.png" alt="Xaviesta Logo" width={3840} height={640} className="w-90 py-2 mx-auto" />
       </div>
-      <div>
-        <h1 className="w-fit mx-auto text-5xl font-mono">VIVO</h1>
+      <div className="w-160 h-19">
+        <Image src="/evereadylogo.png" alt="Evereadylogo Logo" width={1280} height={152} className="object-cover object-center mx-auto" />
       </div>
     </section>
   );
